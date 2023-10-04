9f183e3a7c5baf99eacccfa66240361946f2d486
VozSoldat <muhamaderril7@gmail.com>
2023-10-04 07:45:26 +0700

    update

---
+++ praktikPemilihanII/Pemilihan2Percobaan118.java
@@ -0,0 +1,24 @@
+package praktikPemilihanII;
+
+ import java.util.Scanner;
+
+ 
+ 
+ public class Pemilihan2Percobaan118 {
+ 
+    public static void main(String[] args) {
+        
+        Scanner input18 = new Scanner(System.in);
+        int tahun = input18.nextInt();
+
+        if ((tahun % 4)==0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun kabisat.");
+            }
+        } else {
+            System.out.println("Bukan tahun kabisat.");
+        }
+
+        
+    }
+ }
