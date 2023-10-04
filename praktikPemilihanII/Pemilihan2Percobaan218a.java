fd9f2ff89696616c6dd610a64d194e3bde2f2f4a
VozSoldat <muhamaderril7@gmail.com>
2023-10-04 09:22:26 +0700

    modifikasi

---
+++ praktikPemilihanII/Pemilihan2Percobaan218a.java
@@ -0,0 +1,40 @@
+package praktikPemilihanII;
+import java.util.Scanner;
+
+class Pemilihan2Percobaan218a {
+
+    public static void main(String[] args) {
+        Scanner input18 = new Scanner(System.in);
+        float sudut1, sudut2, sudut3, totalSudut;
+
+        System.out.println("Masukkan sudut 1");
+        sudut1 = input18.nextFloat();
+
+        System.out.println("Masukkan sudut 2");
+        sudut2 = input18.nextFloat();
+
+        System.out.println("Masukkan sudut 3");
+        sudut3 = input18.nextFloat();
+
+        totalSudut = sudut1 + sudut2 + sudut3;
+
+        if (totalSudut == 180) {
+            if (sudut1 == 90 || sudut2 == 90 || sudut3 == 90) {
+                System.out.println("Segitiga siku-siku. ");
+            } else {
+                if (sudut1 == sudut2 && sudut2 == sudut3) {
+                    System.out.println("Segitiga sama sisi. ");
+                } else {
+                    if (sudut1 == sudut2 || sudut2 == sudut3) {
+                        System.out.println("Segitiga sama kaki.");
+                    } else {
+                        System.out.println("Segitiga sembarang. ");
+                    }
+                }
+            }
+        } else {
+            System.out.println("Bukan segitiga.");
+        }
+
+    }
+}
