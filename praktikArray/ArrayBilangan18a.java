9cda4bc22eeadc186fb09a01b457a2fe842c35f1
VozSoldat <muhamaderril7@gmail.com>
2023-10-25 09:30:11 +0700

    hasil modifikasi

---
+++ praktikArray/ArrayBilangan18a.java
@@ -0,0 +1,16 @@
+package praktikArray;
+
+public class ArrayBilangan18a {
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+        bil[0]=5.0;
+        bil[1]=12867;
+        bil[2]=7.5;
+        bil[3]=2000000;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
---
+++ praktikArray/ArrayBilangan18b.java
@@ -0,0 +1,12 @@
+package praktikArray;
+
+public class ArrayBilangan18b {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i<4;i++) {
+            System.out.println(bil[i]);
+        }
+        
+    }
+}
