7cb401bc514809d8583d461a5f20a74827d20ce6
Muhammad Erril <133947567+VozSoldat@users.noreply.github.com>
2023-11-22 08:09:15 +0700

    update

---
+++ praktikFungsi/TerimaKasih_18.java
@@ -0,0 +1,12 @@
+package Kuliah-Yes.praktikFungsi;
+
+public class TerimaKasih_18 {
+    public static void UcapanTerimaKasih(){
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feelm like I could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
