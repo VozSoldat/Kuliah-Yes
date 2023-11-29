08d00bf5298cfc5158fdb22b8554083b2353318c
Muhammad Erril <133947567+VozSoldat@users.noreply.github.com>
2023-11-29 07:03:10 +0700

    modifikasi

--- praktikRekursif/percobaan2.java
+++ praktikRekursif/percobaan2.java
@@ -15,9 +15,18 @@ public class percobaan2 {
 
         System.out.print("Bilangan yang dihitung: ");
         bilangan = sc.nextInt();
-        System.out.print("Pangkat: ");
+        System.out.print("Hasil perhitungan pangkat: ");
         pangkat = sc.nextInt();
+        for (int i = 1; i <= pangkat; i++) {
+            if (i == pangkat) {
+                System.out.print(bilangan+"x");
+                System.out.print("1");
+            } else {
+                System.out.print(bilangan + "x");
+            }
+            
+        }
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print("="+hitungPangkat(bilangan, pangkat));
     }
 }
